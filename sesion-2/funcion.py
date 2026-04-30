52e29ee5053e5a315f74b018d9719350b002d2f5
Henry Vega <hp.vega@hotmail.com>
2026-04-29 21:02:29 -0500

    Add initial project configuration files and main Python script

---
+++ sesion-2/funcion.py
@@ -0,0 +1,9 @@
+def saludo_inicial(nombre):
+    saludo = f"Hola Mundo, soy {nombre} un gusto saludate."
+    return saludo
+
+saludo = saludo_inicial("Luis")
+print(saludo)
+
+saludo_2 = saludo_inicial("Israel")
+print(saludo_2)
