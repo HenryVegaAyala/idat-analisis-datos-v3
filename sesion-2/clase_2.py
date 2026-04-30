e499147bf4ae34a9d5562b39359426b66ac07c75
Henry Vega <hp.vega@hotmail.com>
2026-04-29 22:00:46 -0500

    Add comments to explain function parameters and return value in funcion.py

--- sesion-2/clase_2.py
+++ sesion-2/clase_2.py
@@ -17,3 +17,8 @@ print(f"Soy {gato.nombre} y soy un {gato.especie} y hago {gato.hacer_sonido()}")
 
 perro = Animal("Mailo", "Canino")
 print(f"Soy {perro.nombre} y soy un {perro.especie} y hago {perro.hacer_sonido()}")
+
+nombre = input("Ingrese el nombre del animal: ")
+especie = input("Ingrese la especie del animal (Felino/Canino): ")
+animal = Animal(nombre, especie)
+print(f"Soy {animal.nombre} y soy un {animal.especie} y hago {animal.hacer_sonido()}")
