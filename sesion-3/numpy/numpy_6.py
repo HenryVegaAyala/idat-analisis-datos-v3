7b2ed0613eef70c142a5f2779bf43c3df74a04af
Henry Vega <hp.vega@hotmail.com>
2026-05-04 21:51:36 -0500

    Add numpy script to filter and display anomalous temperature values

---
+++ sesion-3/numpy/numpy_6.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+temperaturas = [22, 21, 23, 90, 22, 20, 105, 21]
+
+# Convirtiendo a un array
+temperaturas_array = np.array(temperaturas)
+
+# Filtrando las temperaturas anómalas (mayores a 50 grados)
+filtrado_anomalo = temperaturas_array > 50
+
+print(f"Valores originales: {temperaturas_array}")
+print(f"Valores filtrados (anómalos): {temperaturas_array[filtrado_anomalo]}")
+print(f"Cantidad de valores anómalos: {temperaturas_array[filtrado_anomalo].size}")
