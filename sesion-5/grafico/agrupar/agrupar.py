ecc2d6fa76d9367a021892b9ceb6aa2af782f45a
Henry Vega <hp.vega@hotmail.com>
2026-05-11 21:57:06 -0500

    Add XML configuration files and Python scripts for CSV data processing

--- sesion-5/grafico/agrupar/agrupar.py
+++ sesion-5/grafico/agrupar/agrupar.py
@@ -9,18 +9,57 @@ cliente = pd.read_csv("clientes.csv")
 resultado = pd.merge(facturacion, cliente, on="id_cliente", how="left")
 
 agrupador = resultado.groupby(["nombre", "pais"]).sum()["monto_total"]
-print(agrupador)
 
 # reiniciar indices
 df_grafico = agrupador.reset_index()
 
 df_grafico["etiqueta"] = df_grafico["nombre"] + " - " + df_grafico["pais"]
 
-print(df_grafico)
-
 # Visualización grafica
 plt.bar(df_grafico["etiqueta"], df_grafico["monto_total"], color=["green", "blue", "red"])
 plt.xlabel("Clientes")
 plt.ylabel("Monto Total")
 plt.title("Facturación por cliente y País")
+plt.show()
+
+print("*" * 20)
+
+barras = plt.bar(
+    df_grafico["etiqueta"],
+    df_grafico["monto_total"],
+    color=["green", "blue", "red"],
+)
+
+for barra in barras:
+    altura = barra.get_height()
+
+    plt.text(
+        barra.get_x() + barra.get_width() / 2,
+        altura,
+        f"{altura:.2f}",
+        ha="center",
+        va="bottom",
+        fontsize=10,
+        fontweight="bold"
+    )
+
+# Linea de tendencia
+plt.plot(
+    df_grafico["etiqueta"],
+    df_grafico["monto_total"],
+    color="orange",
+    marker="o",
+    linestyle="--",
+    linewidth=2,
+    label="Tendencia"
+)
+
+plt.xlabel("Clientes")
+plt.ylabel("Monto Total")
+plt.title("Facturación por cliente y País")
+
+plt.legend()
+plt.xticks(rotation=20)
+plt.tight_layout()
+
 plt.show()
